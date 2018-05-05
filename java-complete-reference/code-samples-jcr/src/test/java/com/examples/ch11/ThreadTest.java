aca32b83545185a674af2bbd4b71d139b7a3c0a0
Lalitanand Dandge <dandgelb@gmail.com>
2018-05-05 23:38:48 +0530

    synchronized keyword

--- java-complete-reference/code-samples-jcr/src/test/java/com/examples/ch11/ThreadTest.java
+++ java-complete-reference/code-samples-jcr/src/test/java/com/examples/ch11/ThreadTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 
 class DemoRunner implements Runnable {
 	@Override
-	public void run() {		
+	public void run() {
 		for(int i = 0; i < 5; i++) {
 			try {
 				System.out.println("Runnable Thread: " + Thread.currentThread().getName() + " : " + i);
@@ -35,6 +35,31 @@ class DemoThread extends Thread {
 	}
 }
 
+class MessagePrinter {
+	synchronized void print(String msg) throws InterruptedException {
+		System.out.print("[ " + msg);
+		Thread.sleep(new Random().nextInt(1000));
+		System.out.println(" ]");
+	}
+}
+
+class MessagePrinterThread implements Runnable {
+	MessagePrinter printer;
+	String msg;
+	public MessagePrinterThread(MessagePrinter printer, String msg) {
+		this.msg = msg;
+		this.printer = printer;
+	}
+	@Override
+	public void run() {
+		try {
+			this.printer.print(this.msg);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
 
 public class ThreadTest {
 	@Test
@@ -114,9 +139,28 @@ public class ThreadTest {
 		});		
 	}
 	
-	@Test
+	//@Test
 	public void testRandomIntegerStream() {
 		Random r = new Random();
 		r.ints(1, 100).forEach(System.out::println);
+	}
+	@Test
+	public void testSynchronized() {
+		MessagePrinter printer = new MessagePrinter();
+		Thread t1 = new Thread(new MessagePrinterThread(printer,"hello")) ;
+		Thread t2 = new Thread(new MessagePrinterThread(printer,"synchronized")) ;
+		Thread t3 = new Thread(new MessagePrinterThread(printer,"world")) ;
+		t1.start();
+		t2.start();
+		t3.start();
+
+		try {
+			t1.join();
+			t2.join();
+			t3.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
