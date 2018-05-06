46ac5d0a21776df350547032e90bed84e48ab5aa
Lalitanand Dandge <dandgelb@gmail.com>
2018-05-06 19:09:32 +0530

    Handle printing redundant permutations if input has duplicates

--- java-complete-reference/code-samples-jcr/src/test/java/com/examples/algorithms/PermutationTest.java
+++ java-complete-reference/code-samples-jcr/src/test/java/com/examples/algorithms/PermutationTest.java
@@ -3,20 +3,27 @@ package com.examples.algorithms;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.Vector;
 
 class Util {
 
     public void permute(Vector<String> v) {
         Vector<String> chosen = new Vector<>();
-        permuteHelper(v, chosen);
+        Set<Vector<String>> printed = new HashSet<>();
+        permuteHelper(v, chosen, printed);
     }
     //{a, b, c, d}
-    public void permuteHelper(Vector<String> v, Vector<String> chosen) {
-        System.out.println("permute v = " + v + ", chosen = " + chosen);
+    public void permuteHelper(Vector<String> v, Vector<String> chosen, Set<Vector<String>> printed) {
+        //System.out.println("permute v = " + v + ", chosen = " + chosen);
         if(v.isEmpty()) {
             //base case
-            System.out.println(chosen);
+            if(!printed.contains(chosen)) {
+                System.out.println(chosen);
+                printed.add(chosen);
+            }
+
         } else {
             //for each choice:
             for (int i = 0; i < v.size(); i++) {
@@ -25,7 +32,7 @@ class Util {
                 chosen.add(s);   // {a}
                 v.remove(i);            // {b, c, d}
                 // - explore
-                permuteHelper(v, chosen);
+                permuteHelper(v, chosen, printed);
                 // - un-choose
                 chosen.remove(chosen.size() - 1);
                 v.insertElementAt(s, i);
@@ -41,7 +48,7 @@ public class PermutationTest {
         //output all possible rearrangements/permutations of the strings in vector
         //What is the vector that is easy to permute - empty vector - super easy
         Util util  = new Util();
-        String [] array = {"M", "A", "R", "T", "Y"};
+        String [] array = {"a", "b", "b", "a", "c"};//{"M", "A", "R", "T", "Y"};
         Vector<String> v = new Vector<>(Arrays.asList(array));
         util.permute(v);
     }
